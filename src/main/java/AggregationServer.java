8b0e9d01c19c5140a9fa3e2a3ec62d208bf72d37
MylesWatkinson <myleswatkinson1@gmail.com>
2023-08-31 12:30:35 +0930

    Adding files

---
+++ src/main/java/AggregationServer.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
+//use HTTPUrl connection
+
+//use server socket
+
+public class AggregationServer
+{
+
+    public static void main(String[] args)
+    {
+
+        int port = 5649;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+ 
+            System.out.println("Server is listening on port " + port);
+ 
+            while (true) {
+                Socket socket = serverSocket.accept();
+ 
+                System.out.println("New client connected");
+ 
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+ 
+                writer.println(new Date().toString());
+            }
+ 
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/QuickTest.java
@@ -0,0 +1,21 @@
+
+package server;
+
+public class QuickTest
+{
+    public static void main()
+    {
+        Server server = new Server("localhost", 8025, "/", null, AggregationServer.class);
+
+        try {
+            server.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Please press a key to stop the server.");
+            reader.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            server.stop();
+        }
+    }
+}
